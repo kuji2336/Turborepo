c6d7441c564cb1a26448cc6618efa516691c2885
Kuji Bolkvadze <58769030+kuji2336@users.noreply.github.com>
2022-10-25 16:17:19 +0200

    added styled components in root dep

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const Button1 = styled.button`
+  background-color: red;
+`
 export const Button = () => {
-  return <button style={{color : 'green'}}>Boop changed to Click</button>;
+  return <Button1 style={{color : 'green'}}>Boop changed to Click</Button1>;
 };
