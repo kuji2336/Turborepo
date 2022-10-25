34a579d05847f89d6d9bc38b4ed4f517abf286e0
Kuji Bolkvadze <58769030+kuji2336@users.noreply.github.com>
2022-10-25 14:02:00 +0200

    changed button text

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button style={{color : 'green'}}>Boop</button>;
+  return <button style={{color : 'green'}}>Boop changed to Click</button>;
 };
